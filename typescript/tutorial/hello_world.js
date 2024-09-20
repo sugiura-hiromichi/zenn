1f3d47a11f51da79e001513570e8583ac186d90a
sugiura-hiromichi <sugiura130418@icloud.com>
2024-09-20 10:22:05 +0900

    created typescript tutorials

---
+++ typescript/tutorial/hello_world.js
@@ -0,0 +1,7 @@
+var message = 'Hello, World!';
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+var body = document.createElement('p');
+body.textContent = 'nice body';
+document.body.appendChild(body);
---
+++ typescript/tutorial/hello_world.ts
@@ -0,0 +1,9 @@
+let message: string = 'Hello, World!';
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+
+let body = document.createElement('p');
+body.textContent = 'nice body';
+document.body.appendChild(body);
