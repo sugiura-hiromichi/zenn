7c0358f4215cb7a152765fa22d3db6c98986e9ef
sugiura-hiromichi <sugiura130418@icloud.com>
2024-10-22 14:31:18 +0900

    created gas_with_wasm project in rust

---
+++ rust/gas_with_wasm/src/lib.rs
@@ -0,0 +1,2 @@
+#[no_mangle]
+pub fn add(left: u64, right: u64,) -> u64 { left + right }
---
+++ rust/gas_with_wasm/target/wasm32-unknown-unknown/debug/libgas_with_wasm.d
@@ -0,0 +1,1 @@
+/Users/a/Downloads/QwQ/rust/gas_with_wasm/target/wasm32-unknown-unknown/debug/libgas_with_wasm.rlib: /Users/a/Downloads/QwQ/rust/gas_with_wasm/src/lib.rs
